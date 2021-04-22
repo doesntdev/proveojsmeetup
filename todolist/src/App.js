a7b8a01365d04223a7c25e865a136fc01be1b2a3
Logan Hart <d.logan.hart@gmail.com>
2021-04-22 15:35:03 -0400

    This is the first commit for the todolist application

---
+++ todolist/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Javascript is the best! Yay.
+          <code> Here's some stuff in code brackets</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
